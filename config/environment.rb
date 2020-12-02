2d2f8dbfb8951ed13d5173314a60bc3eafee26a3
iamShah <formal>
2020-12-02 10:23:12 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,7 @@
 # Require statements should go here.
 
+require_relative '../lib/garden.rb' #these two are the classes that are built here, the more we classes we make
+require_relative '../lib/plant.rb' #the more require relatives we make for them here. so everyime we run run file they will have access to these files
+
 # Then any files that need to load the files required here can
 # require THIS file, environment.rb, and get access to everything
